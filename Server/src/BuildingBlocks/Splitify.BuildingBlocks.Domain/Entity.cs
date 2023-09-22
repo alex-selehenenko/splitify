bf0f63146ee90b6f435e70bcf2dc0896a318619e
o.selehenenko <oleksandr.selehenenko@powercoders.org>
2023-09-22 21:39:19 +0200

    - add BuildingBlocks lib and implemented base entity

---
+++ Server/src/BuildingBlocks/Splitify.BuildingBlocks.Domain/Entity.cs
@@ -0,0 +1,27 @@
+﻿namespace Splitify.BuildingBlocks.Domain
+{
+    public class Entity
+    {
+        public string Id { get; }
+
+        public Entity(string id)
+        {
+            EnsureIdIsValid(id);
+            Id = id;
+
+        }
+
+        private static void EnsureIdIsValid(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                throw new ArgumentNullException(nameof(id));
+            }
+
+            if (id.Length < 1)
+            {
+                throw new ArgumentException("length was less than 1", nameof(id));
+            }
+        }
+    }
+}
