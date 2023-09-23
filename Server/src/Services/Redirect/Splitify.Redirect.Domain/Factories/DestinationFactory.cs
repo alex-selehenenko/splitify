694605b5d6f481a1eda5acbc4359928fb9684a70
o.selehenenko <oleksandr.selehenenko@powercoders.org>
2023-09-23 16:28:54 +0200

    - add DestinationFactory tests and fix url validation

--- Server/src/Services/Redirect/Splitify.Redirect.Domain/Factories/DestinationFactory.cs
+++ Server/src/Services/Redirect/Splitify.Redirect.Domain/Factories/DestinationFactory.cs
@@ -7,11 +7,12 @@ namespace Splitify.Redirect.Domain.Factories
     {
         public Result<Destination> Create(string id, string url, DateTime now)
         {
+            var trimmedUrl = url.Trim();
             var validationResult = ValidateId(id)
-                .Then(res => ValidateUrl(res, url));
+                .Then(res => ValidateUrl(res, trimmedUrl));
             
             return validationResult.IsSuccess
-                ? Result.Success(new Destination(id, now, now, url, 0))
+                ? Result.Success(new Destination(id, now, now, trimmedUrl, 0))
                 : Result.Failure<Destination>(validationResult.Error);
         }
 
@@ -24,7 +25,11 @@ namespace Splitify.Redirect.Domain.Factories
 
         private static Result ValidateUrl(Result result, string url)
         {
-            return Uri.TryCreate(url, UriKind.Absolute, out var uri) & (uri?.Scheme == Uri.UriSchemeHttp || uri?.Scheme == Uri.UriSchemeHttps)
+            var isValidUrl = Uri.TryCreate(url, UriKind.Absolute, out var uri)
+                & (uri?.Scheme == Uri.UriSchemeHttp || uri?.Scheme == Uri.UriSchemeHttps)
+                & uri?.Host.Contains('.');
+
+            return isValidUrl.HasValue && isValidUrl.Value
                 ? result
                 : Result.Failure(DomainError.ValidationError(detail: $"Invalid url - {url}"));
         }
---
+++ Server/src/Services/Redirect/Splitify.Redirect.UnitTests/Domain/DestinationFactoryTests.cs
@@ -0,0 +1,62 @@
+﻿using Splitify.Redirect.Domain.Factories;
+
+namespace Splitify.Redirect.UnitTests.Domain
+{
+    public class DestinationFactoryTests
+    {
+        [Test]
+        [TestCase("http://google.com")]
+        [TestCase("https://google.com")]
+        public void Create_ValidUrl_Success(string url)
+        {
+            var destinationFactory = new DestinationFactory();
+
+            var actual = destinationFactory.Create("dest", url, DateTime.Now);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(actual.IsSuccess, Is.True);
+                Assert.That(actual.Value, Is.Not.Null);
+                Assert.That(actual.Value.Id, Is.EqualTo("dest"));
+                Assert.That(actual.Value.Url, Is.EqualTo(url));
+            });
+        }
+
+        [Test]
+        [TestCase(" https://google.com")]
+        [TestCase("https://google.com ")]
+        public void Create_ValidUrlWithLeadingTrailingSpaces_UrlsAreTrimmed(string url)
+        {
+            var expectedUrl = url.Trim();
+            var destinationFactory = new DestinationFactory();
+
+            var actual = destinationFactory.Create("dest", url, DateTime.Now);
+            
+            Assert.Multiple(() =>
+            {
+                Assert.That(actual.IsSuccess, Is.True);
+                Assert.That(actual.Value, Is.Not.Null);
+                Assert.That(actual.Value.Id, Is.EqualTo("dest"));
+                Assert.That(actual.Value.Url, Is.EqualTo(expectedUrl));
+            });
+        }
+
+        [Test]
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase("\n")]
+        [TestCase("https:/google.com")]
+        [TestCase("https://google")]
+        public void Create_InvalidUrl_Failed(string url)
+        {
+            var destinationFactory = new DestinationFactory();
+
+            var actual = destinationFactory.Create("dest", url, DateTime.Now);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(actual.IsSuccess, Is.False);
+            });
+        }
+    }
+}
