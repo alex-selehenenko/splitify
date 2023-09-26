cea1ca08b7919ee82aa1f98eb197a1e6c69f98e7
o.selehenenko <oleksandr.selehenenko@powercoders.org>
2023-09-26 20:14:30 +0200

    - add create redirect command

---
+++ Server/src/Services/Redirect/Splitify.Redirect.Application/Commands/CreateRedirectionCommand.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Resulty;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Splitify.Redirect.Application.Commands
+{
+    public class CreateRedirectionCommand : IRequest<Result>
+    {
+        public string RedirectId { get; }
+
+        public IEnumerable<string> DestinationUrls { get; }
+
+        public CreateRedirectionCommand(string redirectId, IEnumerable<string> destinationUrls)
+        {
+            RedirectId = redirectId ?? throw new ArgumentNullException(nameof(redirectId));
+            DestinationUrls = destinationUrls ?? throw new ArgumentNullException(nameof(destinationUrls));
+        }
+    }
+}
---
+++ Server/src/Services/Redirect/Splitify.Redirect.Application/Commands/CreatedRedirectCommandHandler.cs
@@ -0,0 +1,44 @@
+﻿using MediatR;
+using Resulty;
+using Splitify.Redirect.Domain;
+using Splitify.Redirect.Domain.Factories;
+using Splitify.Shared.Services.Misc;
+
+namespace Splitify.Redirect.Application.Commands
+{
+    public class CreatedRedirectCommandHandler : IRequestHandler<CreateRedirectionCommand, Result>
+    {
+        private readonly IRedirectionRepository _redirectionRepository;
+        private readonly IDateTimeService _dateTimeService;
+
+        public async Task<Result> Handle(CreateRedirectionCommand request, CancellationToken cancellationToken)
+        {
+            var destinationFactory = new DestinationFactory();
+            var redirectionFactory = new RedirectionFactory();
+
+            var destinations = new List<Destination>();
+            foreach (var url in request.DestinationUrls)
+            {
+                var creationResult = destinationFactory.Create(Guid.NewGuid().ToString(), url, _dateTimeService.UtcNow);
+                if (creationResult.IsFailure)
+                {
+                    return Result.Failure(creationResult.Error);
+                }
+
+                destinations.Add(creationResult.Value);
+            }
+
+            var redirectionCreationResult = redirectionFactory.Create(request.RedirectId, destinations, _dateTimeService.UtcNow);
+
+            if (redirectionCreationResult.IsFailure)
+            {
+                return Result.Failure(redirectionCreationResult.Error);
+            }
+
+            _redirectionRepository.Add(redirectionCreationResult.Value);
+            await _redirectionRepository.UnitOfWork.SaveChangesAsync(cancellationToken);
+
+            return Result.Success();
+        }
+    }
+}
