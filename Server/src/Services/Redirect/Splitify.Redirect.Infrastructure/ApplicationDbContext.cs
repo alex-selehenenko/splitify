cc8ac2db8c3485ee1a2c1a0333e1f6ad2599ac1f
o.selehenenko <oleksandr.selehenenko@powercoders.org>
2023-09-26 15:55:41 +0200

    - add DbContext and entity configuration abstractions

---
+++ Server/src/Services/Redirect/Splitify.Redirect.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Splitify.BuildingBlocks.Domain.Persistence;
+using Splitify.Redirect.Domain;
+
+namespace Splitify.Redirect.Infrastructure
+{
+    public class ApplicationDbContext : DbContext, IUnitOfWork
+    {
+        public DbSet<Redirection> Redirections { get; set; }
+
+        public DbSet<Destination> Destinations { get; set; }
+    }
+}
---
+++ Server/src/Services/Redirect/Splitify.Redirect.Infrastructure/EntityConfiguration/Abstractions/EntityConfigurationBase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Splitify.BuildingBlocks.Domain;
+
+namespace Splitify.Redirect.Infrastructure.EntityConfiguration.Abstractions
+{
+    public abstract class EntityConfigurationBase<TEntity> : IEntityTypeConfiguration<TEntity>
+        where TEntity : Entity
+    {
+        public virtual void Configure(EntityTypeBuilder<TEntity> builder)
+        {
+            builder.HasKey(e => e.Id);
+
+            builder.Property(e => e.Id);
+
+            builder.Ignore(entity => entity.Events);
+        }
+    }
+}
