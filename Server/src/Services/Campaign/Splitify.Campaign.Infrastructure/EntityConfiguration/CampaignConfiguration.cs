c4c002917cbc3d2a32fdffbfe95de04ec4887e9c
o.selehenenko <oleksandr.selehenenko@powercoders.org>
2023-09-27 20:22:42 +0200

    - add campaign entities configuration

---
+++ Server/src/Services/Campaign/Splitify.Campaign.Infrastructure/EntityConfiguration/CampaignConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Splitify.Campaign.Domain;
+using Splitify.Shared.AspDotNet.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Splitify.Campaign.Infrastructure.EntityConfiguration
+{
+    internal class CampaignConfiguration : EntityConfigurationBase<CampaignAggregate>
+    {
+        public override void Configure(EntityTypeBuilder<CampaignAggregate> builder)
+        {
+            base.Configure(builder);
+
+            builder.Property(x => x.IsActive)
+                .UsePropertyAccessMode(PropertyAccessMode.Property);
+
+            builder.HasMany(x => x.Links)
+                .WithOne()
+                .HasForeignKey("CampaignId");
+        }
+    }
+}
---
+++ Server/src/Services/Campaign/Splitify.Campaign.Infrastructure/EntityConfiguration/LinkConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Splitify.Campaign.Domain;
+using Splitify.Shared.AspDotNet.EntityFramework;
+
+namespace Splitify.Campaign.Infrastructure.EntityConfiguration
+{
+    internal class LinkConfiguration : EntityConfigurationBase<Link>
+    {
+        public override void Configure(EntityTypeBuilder<Link> builder)
+        {
+            base.Configure(builder);
+
+            builder.Property(x => x.Url)
+                .UsePropertyAccessMode(PropertyAccessMode.Property);
+        }
+    }
+}
