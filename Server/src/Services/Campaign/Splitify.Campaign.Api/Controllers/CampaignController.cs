3f49a5e58947083b1854f7d6af2768d16499de21
o.selehenenko <oleksandr.selehenenko@powercoders.org>
2023-09-28 13:07:35 +0200

    - implement campaign controller

--- Server/src/Services/Campaign/Splitify.Campaign.Api/Controllers/CampaignController.cs
+++ Server/src/Services/Campaign/Splitify.Campaign.Api/Controllers/CampaignController.cs
@@ -1,4 +1,8 @@
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Resulty;
+using Splitify.Campaign.Application.Commands;
+using Splitify.Shared.AspDotNet.Results;
 
 namespace Splitify.Campaign.Api.Controllers
 {
@@ -6,9 +10,26 @@ namespace Splitify.Campaign.Api.Controllers
     [Route("api/v1/campaigns")]
     public class CampaignController : ControllerBase
     {
-        public CampaignController()
+        private readonly IMediator _mediator;
+
+        public CampaignController(IMediator mediator)
         {
-            
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync([FromBody] CreateCampaignCommand body)
+        {
+            return await _mediator.Send(body)
+                .MapAsync(
+                    result => Created(result.CampaignId, result),
+                    error => CreateProblemResponse(error));
+        }
+
+        private IActionResult CreateProblemResponse(Error error)
+        {
+            var problemDetails = error.ToProblemDetails();
+            return StatusCode(problemDetails.Status ?? 500, problemDetails);
         }
     }
 }
