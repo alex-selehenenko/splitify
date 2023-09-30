53c4f6dd55cb1a82c513cb76136c8f87d951c8ab
o.selehenenko <oleksandr.selehenenko@powercoders.org>
2023-09-30 22:05:52 +0200

    - rework data fetching

--- Client/src/campaigns/script.js
+++ Client/src/campaigns/script.js
@@ -23,16 +23,6 @@ class Application{
     
     constructor(){
         this.campaigns = [];
-        this.campaignsLoadedEventType = 'campaignsLoaded'
-    }
-
-    run(){
-        this.fetchCampaigns()
-            .then(data => {
-                this.campaigns = data;
-                this.dispatchEvent(this.campaignsLoadedEventType);               
-            })
-            .catch(error => console.log(error));
     }
 
     async fetchJson(path){
@@ -49,12 +39,6 @@ class Application{
     async fetchCampaigns(){
         return await this.fetchJson(campaignsPath);
     }
-
-    dispatchEvent(eventType){
-        const event = new Event(eventType);
-        document.dispatchEvent(event);
-    }
 }
 
 const app = new Application();
-app.run();
