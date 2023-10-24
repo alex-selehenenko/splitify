40a64b55b7527df744f2839236307d4e7120887d
o.selehenenko <oleksandr.selehenenko@powercoders.org>
2023-10-24 08:20:02 +0200

    - add health controller

---
+++ Server/src/Services/Identity/Splitify.Identity.Api/Controllers/Health.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Splitify.Identity.Api.Controllers
+{
+    [Route("api/v1/health")]
+    [ApiController]
+    public class Health : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
