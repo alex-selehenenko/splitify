da618a37885d287492b7394860aef978993c4b90
o.selehenenko <oleksandr.selehenenko@powercoders.org>
2023-10-24 07:52:04 +0200

    - add logs to get user query handler

--- Server/src/Services/Identity/Splitify.Identity.Application/Queries/GetUserQueryHandler.cs
+++ Server/src/Services/Identity/Splitify.Identity.Application/Queries/GetUserQueryHandler.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using Microsoft.Extensions.Logging;
 using Resulty;
 using Splitify.BuildingBlocks.Domain.Errors;
 using Splitify.Identity.Application.Queries.Dto;
@@ -11,15 +12,22 @@ namespace Splitify.Identity.Application.Queries
     {
         private readonly IUserService _userService;
         private readonly IUserRepository _userRepository;
+        private readonly ILogger<GetUserQueryHandler> _logger;
 
-        public GetUserQueryHandler(IUserService userService, IUserRepository userRepository)
+        public GetUserQueryHandler(
+            IUserService userService,
+            IUserRepository userRepository,
+            ILogger<GetUserQueryHandler> logger)
         {
+            _logger = logger;
             _userService = userService;
             _userRepository = userRepository;
         }
 
         public async Task<Result<GetUserDto>> Handle(GetUserQuery request, CancellationToken cancellationToken)
         {
+            _logger.LogInformation("Executing get user query for user id - {id}", _userService.GetUserId());
+
             var userId = _userService.GetUserId();
             var user = await _userRepository.FindAsync(userId, cancellationToken);
 
