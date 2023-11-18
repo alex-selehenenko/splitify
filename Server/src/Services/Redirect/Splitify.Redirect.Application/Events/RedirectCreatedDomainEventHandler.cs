936f9e0d30a596f758773d186d7f22c802ecb1b0
o.selehenenko <oleksandr.selehenenko@powercoders.org>
2023-11-18 19:16:09 +0100

    - add logs

--- Server/src/Services/Redirect/Splitify.Redirect.Application/Events/RedirectCreatedDomainEventHandler.cs
+++ Server/src/Services/Redirect/Splitify.Redirect.Application/Events/RedirectCreatedDomainEventHandler.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using Microsoft.Extensions.Logging;
 using Splitify.BuildingBlocks.EventBus;
 using Splitify.EventBus.Contracts;
 using Splitify.Redirect.Domain.Events;
@@ -8,16 +9,21 @@ namespace Splitify.Redirect.Application.Events
     public class RedirectCreatedDomainEventHandler : INotificationHandler<RedirectCreatedDomainEvent>
     {
         private readonly IEventBus _eventBus;
+        private readonly ILogger<RedirectCreatedDomainEventHandler> _logger;
 
-        public RedirectCreatedDomainEventHandler(IEventBus eventBus)
+        public RedirectCreatedDomainEventHandler(IEventBus eventBus, ILogger<RedirectCreatedDomainEventHandler> logger)
         {
             _eventBus = eventBus;
+            _logger = logger;
         }
 
         public async Task Handle(RedirectCreatedDomainEvent notification, CancellationToken cancellationToken)
         {
+            _logger.LogInformation("Sending message RedirectCreated for redirect - {id}", notification.CampaignId);
             var message = new RedirectCreatedMessage(notification.CampaignId);
             await _eventBus.PublishAsync(message, cancellationToken);
+
+            _logger.LogInformation("Success: Message RedirectCreated for redirect - {id} has been sent", notification.CampaignId);
         }
     }
 }
